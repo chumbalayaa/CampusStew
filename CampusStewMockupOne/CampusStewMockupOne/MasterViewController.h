9ae104edb07a88f588719f62a53ceb3f0f79f61c
HarryRein <hreinmit@gmail.com>
2013-06-01 21:56:40 -0400

    overhaul

---
+++ CampusStewMockupOne/CampusStewMockupOne/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  CampusStewMockupOne
+//
+//  Created by Harry Rein on 5/31/13.
+//  Copyright (c) 2013 PPP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
