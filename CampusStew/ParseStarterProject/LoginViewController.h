baeb961e84db63e0444793e3966e744e29f2df99
Alex Chumbley <Chumbley@mit.edu>
2013-06-01 20:01:24 -0400

    initial commit

---
+++ CampusStew/ParseStarterProject/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  CampusStew
+//
+//  Created by Alexander Chumbley on 6/1/13.
+//
+//
+
+#import <Parse/Parse.h>
+
+@interface LoginViewController : PFLogInViewController
+
+@end
---
+++ CampusStew/ParseStarterProject/LoginViewController.m
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.m
+//  CampusStew
+//
+//  Created by Alexander Chumbley on 6/1/13.
+//
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    self.view.backgroundColor = [UIColor blueColor];
+    
+    self.logInView.usernameField.backgroundColor = [UIColor redColor];
+    self.logInView.passwordField.backgroundColor = [UIColor greenColor];
+    
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
